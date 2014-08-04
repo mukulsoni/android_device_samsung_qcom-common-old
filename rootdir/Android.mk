32c409a7b94aa870ad3239e2bacde70d370024c5
Mukul Soni <mukulsoni2@gmail.com>
2014-08-05 02:35:42 +0700

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -88,14 +88,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init_msm8226.c
-LOCAL_MODULE_TAGS := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init_msm8226.c
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.ril.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
